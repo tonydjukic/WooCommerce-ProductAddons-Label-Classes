c4093f713d8c1a56c17360b61b710686098234a4
Tony Djukic <tony@endlessdigital.com>
2014-05-24 13:38:06 -0400

    Update custom_price.php
    Updated to match WC Product Add-ons v2.6.2

--- custom_price.php
+++ custom_price.php
@@ -1,20 +1,15 @@
 <?php foreach ( $addon['options'] as $key => $option ) :
+	$addon_key     = 'addon-' . sanitize_title( $addon['field-name'] );
+	$option_key    = empty( $option['label'] ) ? $key : sanitize_title( $option['label'] );
+	$current_value = isset( $_POST[ $addon_key ] ) && isset( $_POST[ $addon_key ][ $option_key ] ) ? $_POST[ $addon_key ][ $option_key ] : '';
+	$price         = $option['price'] > 0 ? '(' . woocommerce_price( $option['price'] ) . ')' : '';
+	?>
 
-	$current_value = isset( $_POST[ 'addon-' . sanitize_title( $addon['field-name'] ) . '-' . sanitize_title( $option['label'] ) ] ) ? $_POST[ 'addon-' . sanitize_title( $addon['field-name'] ) . '-' . sanitize_title( $option['label'] ) ] : '';
-	$price = $option['price'] > 0 ? '(' . woocommerce_price( $option['price'] ) . ')' : '';
-
-	if ( empty( $option['label'] ) ) : ?>
-
-		<p class="form-row form-row-wide addon-wrap-<?php echo sanitize_title( $addon['field-name'] ); ?>">
-			<input type="number" step="any" class="input-text addon addon-custom-price" name="addon-<?php echo sanitize_title( $addon['field-name'] ); ?>-<?php echo sanitize_title( $option['label'] ); ?>" value="<?php echo esc_attr( $current_value ); ?>" <?php if ( ! empty( $option['min'] ) ) echo 'min="' . $option['min'] .'"'; ?> <?php if ( ! empty( $option['max'] ) ) echo 'max="' . $option['max'] .'"'; ?> />
-		</p>
-
-	<?php else : ?>
-
-		<p class="form-row form-row-wide addon-wrap-<?php echo sanitize_title( $addon['field-name'] ); ?>">
-			<label class="addonlabel-<?php echo sanitize_title( $option['label'] ); ?>"><?php echo wptexturize( $option['label'] ) . ' ' . $price; ?> <input type="number" step="any" class="input-text addon addon-custom-price" name="addon-<?php echo sanitize_title( $addon['field-name'] ); ?>-<?php echo sanitize_title( $option['label'] ); ?>" value="<?php echo esc_attr( $current_value ); ?>" <?php if ( ! empty( $option['min'] ) ) echo 'min="' . $option['min'] .'"'; ?> <?php if ( ! empty( $option['max'] ) ) echo 'max="' . $option['max'] .'"'; ?> /></label>
-		</p>
-
-	<?php endif; ?>
+	<p class="form-row form-row-wide addon-wrap-<?php echo sanitize_title( $addon['field-name'] ); ?>">
+		<?php if ( ! empty( $option['label'] ) ) : ?>
+			<label class="addonlabel-<?php echo sanitize_title( $option['label'] ); ?>"><?php echo wptexturize( $option['label'] ) . ' ' . $price; ?></label>
+		<?php endif; ?>
+		<input type="number" step="any" class="input-text addon addon-custom-price" name="<?php echo $addon_key ?>[<?php echo $option_key; ?>]" value="<?php echo esc_attr( $current_value ); ?>" <?php if ( ! empty( $option['min'] ) ) echo 'min="' . $option['min'] .'"'; ?> <?php if ( ! empty( $option['max'] ) ) echo 'max="' . $option['max'] .'"'; ?> />
+	</p>
 
 <?php endforeach; ?>
