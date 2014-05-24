0a3d3a1c8ce2eaf1caca9d00786dcfcf78a8428c
Tony Djukic <tony@endlessdigital.com>
2014-05-24 13:37:18 -0400

    Update custom_textarea.php
    Updated to match WC Product Add-ons v2.6.2

--- custom_textarea.php
+++ custom_textarea.php
@@ -1,19 +1,15 @@
 <?php foreach ( $addon['options'] as $key => $option ) :
-	$current_value = isset( $_POST[ 'addon-' . sanitize_title( $addon['field-name'] ) . '-' . sanitize_title( $option['label'] ) ] ) ? $_POST[ 'addon-' . sanitize_title( $addon['field-name'] ) . '-' . sanitize_title( $option['label'] ) ] : '';
-	$price = $option['price'] > 0 ? '(' . woocommerce_price( $option['price'] ) . ')' : '';
+	$addon_key     = 'addon-' . sanitize_title( $addon['field-name'] );
+	$option_key    = empty( $option['label'] ) ? $key : sanitize_title( $option['label'] );
+	$current_value = isset( $_POST[ $addon_key ] ) && isset( $_POST[ $addon_key ][ $option_key ] ) ? $_POST[ $addon_key ][ $option_key ] : '';
+	$price         = $option['price'] > 0 ? '(' . woocommerce_price( $option['price'] ) . ')' : '';
+	?>
 
-	if ( empty( $option['label'] ) ) : ?>
-
-		<p class="form-row form-row-wide addon-wrap-<?php echo sanitize_title( $addon['field-name'] ); ?>">
-			<textarea type="text" class="input-text addon addon-custom-textarea" data-price="<?php echo $option['price']; ?>" name="addon-<?php echo sanitize_title( $addon['field-name'] ); ?>-<?php echo sanitize_title( $option['label'] ); ?>" rows="4" cols="20" <?php if ( ! empty( $option['max'] ) ) echo 'maxlength="' . $option['max'] .'"'; ?>><?php echo esc_textarea( $current_value ); ?></textarea>
-		</p>
-
-	<?php else : ?>
-
-		<p class="form-row form-row-wide addon-wrap-<?php echo sanitize_title( $addon['field-name'] ); ?>">
-			<label class="addonlabel-<?php echo sanitize_title( $option['label'] ); ?>"><?php echo wptexturize( $option['label'] ) . ' ' . $price; ?> <textarea type="text" class="input-text addon addon-custom-textarea" data-price="<?php echo $option['price']; ?>" name="addon-<?php echo sanitize_title( $addon['field-name'] ); ?>-<?php echo sanitize_title( $option['label'] ); ?>" rows="4" cols="20" <?php if ( ! empty( $option['max'] ) ) echo 'maxlength="' . $option['max'] .'"'; ?>><?php echo esc_textarea( $current_value ); ?></textarea></label>
-		</p>
-
-	<?php endif; ?>
+	<p class="form-row form-row-wide addon-wrap-<?php echo sanitize_title( $addon['field-name'] ); ?>">
+		<?php if ( ! empty( $option['label'] ) ) : ?>
+			<label class="addonlabel-<?php echo sanitize_title( $option['label'] ); ?>"><?php echo wptexturize( $option['label'] ) . ' ' . $price; ?></label>
+		<?php endif; ?>
+		<textarea type="text" class="input-text addon addon-custom-textarea" data-price="<?php echo $option['price']; ?>" name="<?php echo $addon_key ?>[<?php echo $option_key; ?>]" rows="4" cols="20" <?php if ( ! empty( $option['max'] ) ) echo 'maxlength="' . $option['max'] .'"'; ?>><?php echo esc_textarea( $current_value ); ?></textarea>
+	</p>
 
 <?php endforeach; ?>
