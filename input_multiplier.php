6e79f6f470512c27454c9a75282768948fb54243
Tony Djukic <tony@endlessdigital.com>
2014-05-24 13:35:50 -0400

    Update input_multiplier.php
    Typo correction in label name prefix.

--- input_multiplier.php
+++ input_multiplier.php
@@ -7,7 +7,7 @@
 
 	<p class="form-row form-row-wide addon-wrap-<?php echo sanitize_title( $addon['field-name'] ); ?>">
 		<?php if ( ! empty( $option['label'] ) ) : ?>
-			<label class="rc-<?php echo sanitize_title( $option['label'] ); ?>"><?php echo wptexturize( $option['label'] ) . ' ' . $price; ?></label>
+			<label class="addonlabel-<?php echo sanitize_title( $option['label'] ); ?>"><?php echo wptexturize( $option['label'] ) . ' ' . $price; ?></label>
 		<?php endif; ?>
 		<input type="number" step="" class="input-text addon addon-input_multiplier" data-price="<?php echo $option['price']; ?>" name="<?php echo $addon_key ?>[<?php echo $option_key; ?>]" value="<?php echo esc_attr( $current_value ); ?>" <?php if ( ! empty( $option['min'] ) || $option['min'] === '0' ) echo 'min="' . $option['min'] .'"'; ?> <?php if ( ! empty( $option['max'] ) ) echo 'max="' . $option['max'] .'"'; ?> />
 		<span class="addon-alert"><?php _e( 'This must be a number!', 'woocommerce-product-addons' ); ?></span>
