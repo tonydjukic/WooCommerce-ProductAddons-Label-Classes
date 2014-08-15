e3cd2f2b48fba48233b62b275d12419a6de58208
Tony Djukic <tony@endlessdigital.com>
2014-08-15 14:25:53 -0400

    Update custom_textarea.php

--- custom_textarea.php
+++ custom_textarea.php
@@ -7,7 +7,7 @@
 
 	<p class="form-row form-row-wide addon-wrap-<?php echo sanitize_title( $addon['field-name'] ); ?>">
 		<?php if ( ! empty( $option['label'] ) ) : ?>
-			<label class="rc-<?php echo sanitize_title( $option['label'] ); ?>"><?php echo wptexturize( $option['label'] ) . ' ' . $price; ?></label>
+			<label class="addonlabel-<?php echo sanitize_title( $option['label'] ); ?>"><?php echo wptexturize( $option['label'] ) . ' ' . $price; ?></label>
 		<?php endif; ?>
 		<textarea type="text" class="input-text addon addon-custom-textarea" data-price="<?php echo get_product_addon_price_for_display( $option['price'] ); ?>" name="<?php echo $addon_key ?>[<?php echo $option_key; ?>]" rows="4" cols="20" <?php if ( ! empty( $option['max'] ) ) echo 'maxlength="' . $option['max'] .'"'; ?>><?php echo esc_textarea( $current_value ); ?></textarea>
 	</p>
