37bae6476a76f6adaee01da7006f5472ea3ddc91
Tony Djukic <tony@endlessdigital.com>
2014-08-15 14:24:05 -0400

    Update checkbox.php

--- checkbox.php
+++ checkbox.php
@@ -1,6 +1,6 @@
 <?php foreach ( $addon['options'] as $i => $option ) :
 
-	$price = $option['price'] > 0 ? '(' . woocommerce_price( $option['price'] ) . ')' : '';
+	$price = $option['price'] > 0 ? '(' . woocommerce_price( get_product_addon_price_for_display( $option['price'] ) ) . ')' : '';
 
 	$current_value = (
 			isset( $_POST[ 'addon-' . sanitize_title( $addon['field-name'] ) ] ) &&
@@ -9,7 +9,7 @@
 	?>
 
 	<p class="form-row form-row-wide addon-wrap-<?php echo sanitize_title( $addon['field-name'] ) . '-' . $i; ?>">
-		<label class="addonlabel-<?php echo sanitize_title( $option['label'] ); ?>"><input type="checkbox" class="addon addon-checkbox" name="addon-<?php echo sanitize_title( $addon['field-name'] ); ?>[]" data-price="<?php echo $option['price']; ?>" value="<?php echo sanitize_title( $option['label'] ); ?>" <?php checked( $current_value, 1 ); ?> /> <?php echo wptexturize( $option['label'] . ' ' . $price ); ?></label>
+		<label class="addonlabel-<?php echo sanitize_title( $option['label'] ); ?>"><input type="checkbox" class="addon addon-checkbox" name="addon-<?php echo sanitize_title( $addon['field-name'] ); ?>[]" data-price="<?php echo get_product_addon_price_for_display( $option['price'] ); ?>" value="<?php echo sanitize_title( $option['label'] ); ?>" <?php checked( $current_value, 1 ); ?> /> <?php echo wptexturize( $option['label'] . ' ' . $price ); ?></label>
 	</p>
 
 <?php endforeach; ?>
