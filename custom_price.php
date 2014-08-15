b28abea33ca033e69feba735df73c6379c8f346a
Tony Djukic <tony@endlessdigital.com>
2014-08-15 14:25:13 -0400

    Update custom_price.php

--- custom_price.php
+++ custom_price.php
@@ -2,7 +2,7 @@
 	$addon_key     = 'addon-' . sanitize_title( $addon['field-name'] );
 	$option_key    = empty( $option['label'] ) ? $key : sanitize_title( $option['label'] );
 	$current_value = isset( $_POST[ $addon_key ] ) && isset( $_POST[ $addon_key ][ $option_key ] ) ? $_POST[ $addon_key ][ $option_key ] : '';
-	$price         = $option['price'] > 0 ? '(' . woocommerce_price( $option['price'] ) . ')' : '';
+	$price         = $option['price'] > 0 ? '(' . woocommerce_price( get_product_addon_price_for_display( $option['price'] ) ) . ')' : '';
 	?>
 
 	<p class="form-row form-row-wide addon-wrap-<?php echo sanitize_title( $addon['field-name'] ); ?>">
