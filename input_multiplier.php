05fd1f9d1d89724a5c0c91f1aa7bef48e3ffd05d
Tony Djukic <tony@endlessdigital.com>
2015-01-29 15:10:42 -0500

    Update input_multiplier.php

--- input_multiplier.php
+++ input_multiplier.php
@@ -8,7 +8,7 @@
 	<p class="form-row form-row-wide addon-wrap-<?php echo sanitize_title( $addon['field-name'] ); ?>">
 		<?php if ( ! empty( $option['label'] ) ) : ?>
 			<label class="addonlabel-<?php echo sanitize_title( $option['label'] ); ?>"><?php echo wptexturize( $option['label'] ) . ' ' . $price; ?></label>
-		<?php endif; ?>
+		<?php endif; ?>	
 		<input type="number" step="" class="input-text addon addon-input_multiplier" data-price="<?php echo get_product_addon_price_for_display( $option['price'] ); ?>" name="<?php echo $addon_key ?>[<?php echo $option_key; ?>]" value="<?php echo ( esc_attr( $current_value ) == '' ? $option['min'] : esc_attr( $current_value ) ); ?>" <?php if ( ! empty( $option['min'] ) || $option['min'] === '0' ) echo 'min="' . $option['min'] .'"'; ?> <?php if ( ! empty( $option['max'] ) ) echo 'max="' . $option['max'] .'"'; ?> />
 		<span class="addon-alert"><?php _e( 'This must be a number!', 'woocommerce-product-addons' ); ?></span>
 	</p>
